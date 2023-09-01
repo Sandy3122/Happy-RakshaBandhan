025df1ffae1b8b2d9dfe47d8b5571555825d930c
Sandy3122 <seeram87@gmail.com>
2023-09-01 11:37:20 +0530

    Added all the files

---
+++ public/script.js
@@ -0,0 +1,7 @@
+document.getElementById("searchBtn").addEventListener("click", function() {
+    const searchedName = document.getElementById("nameSearch").value;
+    if (searchedName) {
+      window.location.href = `greeting.html?name=${searchedName}`;
+    }
+  });
+  
