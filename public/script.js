2ea53a3ba3a8b5ad473663ada5af67d8e4427ae6
Sandy3122 <seeram87@gmail.com>
2023-09-01 11:30:28 +0530

    Removed all the files

--- public/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("searchBtn").addEventListener("click", function() {
-    const searchedName = document.getElementById("nameSearch").value;
-    if (searchedName) {
-      window.location.href = `greeting.html?name=${searchedName}`;
-    }
-  });
-  
